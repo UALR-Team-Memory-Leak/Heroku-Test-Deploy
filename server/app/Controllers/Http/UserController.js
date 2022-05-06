5455b66be8cfd54f41ee0b4917cff63d30bd500b
wgriff6 <willgv557@gmail.com>
2022-05-06 16:34:03 -0500

    installed sqlite 3, ran migrations, user persists

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -1,9 +1,16 @@
 'use strict'
 
+const User = use('App/Models/User');
 class UserController {
-    register({request}) {
+    async register({request}) {
         const {email, username, password, role} = request.all();
-        console.log(email, username, password, role)
+        console.log(email, username, password, role) //prints data to console
+        const user = await User.create({
+            email,
+            username, //can set this to email if we want: username: email,
+            password,
+            role,
+        });
         return{ 
             message: 'hello world',
         };
