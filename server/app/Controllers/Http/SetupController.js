7f1f2657101c44deb62a1aaef9f70630ae29062a
wgriff6 <willgv557@gmail.com>
2022-05-09 10:14:22 -0500

    intialized setup methods

--- server/app/Controllers/Http/SetupController.js
+++ server/app/Controllers/Http/SetupController.js
@@ -4,6 +4,14 @@ class SetupController {
     async setup({request}) {
         return 'Some test';
     }
+
+    async addInstructor({request}) {
+
+    }
+
+    async addSection({}) {
+        
+    }
 }
 
 module.exports = SetupController
